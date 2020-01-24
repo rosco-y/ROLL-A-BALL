a3fec519b2f68043ffd0089a99d5e7ebe8157ff0
Ross Ylitalo <ross.ylitalo@gmail.com>
2020-01-24 15:02:14 -0500

    Picking up pickup objects and counting them and displaying status text.

--- roll a ball/Assets/Scripts/PlayerController.cs
+++ roll a ball/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
+    int _count = 0;
+
     public float _Speed = 2.0f;
     Rigidbody _rBody;
+    public Text _CountText;
+    
     void Start()
     {
         _rBody = GetComponent<Rigidbody>();
+        _count = 0;
+        displayScore();
+    }
+
+    void displayScore()
+    {
+        if (_count < 23)
+            _CountText.text = $"Count: {_count}";
+        else
+            _CountText.text = "You Win!!!";
+        
     }
 
     // Update is called once per frame
@@ -19,4 +34,15 @@ public class PlayerController : MonoBehaviour
 
         _rBody.AddForce(new Vector3(h * _Speed, 0f, v * _Speed));
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            displayScore();
+        }
+    }
+
 }
