e035dca6fa851e0422adc641a9c17d96e2c9783b
Ross Ylitalo <ross.ylitalo@gmail.com>
2020-01-24 13:05:18 -0500

    Created pickups

---
+++ roll a ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject _Player;
+    Vector3 _offset;
+    void Start()
+    {
+        _offset = _Player.transform.position - transform.position;
+    }
+    void LateUpdate()
+    {
+        transform.position = _Player.transform.position - _offset;
+    }
+}
---
+++ roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float _Speed = 2.0f;
+    Rigidbody _rBody;
+    void Start()
+    {
+        _rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        _rBody.AddForce(new Vector3(h * _Speed, 0f, v * _Speed));
+    }
+}
